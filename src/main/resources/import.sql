5779a0a8c5b05239233fcfb4769abd39e9f57d24
Davide Cangiano <cangiano.0497@gmail.com>
2023-09-28 19:14:33 +0200

    Added offer icon and changed badge color

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -19,3 +19,4 @@ INSERT INTO db_pizza.ingredients(name)VALUES('Rosmarino');
 INSERT INTO db_pizza.ingredients(name)VALUES('Ricotta');
 INSERT INTO db_pizza.ingredients(name)VALUES('Pepe');
 INSERT INTO db_pizza.ingredients(name)VALUES('Provola');
+INSERT INTO db_pizza.ingredients(name)VALUES('Rucola');
