a5a7a6373f6375c83b044aab7e5ca2cefce2735a
Davide Cangiano <cangiano.0497@gmail.com>
2023-09-28 18:29:46 +0200

    Upgraded detail page and pizza list

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,10 +3,19 @@ insert into pizze(name, description, price)values('Fritta', 'Completa', 7.50)
 
 insert into pizze(name, description, price)values('Margherita', 'Classica', 6.50)
 
+insert into pizze(name, description, price)values('Marinara', 'Classica', 6.50)
 
 insert into pizze(name, description, price)values('Tronchetto', 'con rucola e formaggio', 8.50)
+
+insert into pizze(name, description, price)values('Americana', 'Bianca', 8.50)
 
 
 INSERT INTO db_pizza.ingredients(name)VALUES('Mozzarella');
 INSERT INTO db_pizza.ingredients(name)VALUES('Basilico');
 INSERT INTO db_pizza.ingredients(name)VALUES('Pomodoro');
+INSERT INTO db_pizza.ingredients(name)VALUES('Patatine');
+INSERT INTO db_pizza.ingredients(name)VALUES('Aglio');
+INSERT INTO db_pizza.ingredients(name)VALUES('Rosmarino');
+INSERT INTO db_pizza.ingredients(name)VALUES('Ricotta');
+INSERT INTO db_pizza.ingredients(name)VALUES('Pepe');
+INSERT INTO db_pizza.ingredients(name)VALUES('Provola');
